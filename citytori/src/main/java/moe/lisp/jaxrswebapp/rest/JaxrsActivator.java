1f7aeb41815a62a22790eeaf5c26a26c4faec827
unknown <>
2015-10-30 17:12:11 +0900

    edit citytori project

--- citytori/src/main/java/moe/lisp/jaxrswebapp/rest/JaxrsActivator.java
+++ citytori/src/main/java/moe/lisp/jaxrswebapp/rest/JaxrsActivator.java
@@ -3,6 +3,6 @@ package moe.lisp.jaxrswebapp.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("rest")
+@ApplicationPath("api")
 public class JaxrsActivator extends Application{
 }
