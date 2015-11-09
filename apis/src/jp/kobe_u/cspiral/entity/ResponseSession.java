82b676ed722efbfc1fc070d7ce3d9673ad756d99
unknown <>
2015-11-09 21:41:07 +0900

    repair Session API

--- apis/src/jp/kobe_u/cspiral/entity/ResponseSession.java
+++ apis/src/jp/kobe_u/cspiral/entity/ResponseSession.java
@@ -1,7 +1,9 @@
 package jp.kobe_u.cspiral.entity;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name="session")
 public class ResponseSession {
 
 	private String userId;
